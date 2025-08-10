9721f5bd04034f70395942f6ccd61e48e448d147
ak5028 <aj4467507@gmail.com>
2025-08-10 12:37:23 +0530

    Create C-Make It Beautiful.cpp
    https://codeforces.com/contest/2118/problem/C

---
+++ C-Make It Beautiful.cpp
@@ -0,0 +1,26 @@
+//https://codeforces.com/contest/2118/problem/C
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ll t;cin>>t;
+    while(t--){
+        ll n,k;cin>>n>>k;
+        ll a[n];ll s,ans=0;
+        for(int i=0;i<n;i++){cin>>a[i];}
+        for(int j=0;j<60;j++){
+            for(int i=0;i<n;i++){
+                if(a[i]>>j &1){ans++;}
+                else{
+                    s=1ll<<j;
+                    if(k>=s){
+                        ans++;
+                        k-=s;
+                    }
+                }
+            }
+        }
+        cout<<ans<<endl;
+    }
+}
